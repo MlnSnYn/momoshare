5fc65bc80b3276a1c194414fdf31a686d39a0169
MlnSnYn <96682914+MlnSnYn@users.noreply.github.com>
2024-03-08 08:55:53 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=34143429&pid=71a93b75a4a016ad4ef6b8243b89d7a1&tid=8d352c974d85f76c511b62ff3fc9b5f1",
+    "https://www.maimemo.com/share/page?uid=34143429&pid=54c96370dff75dffbfb115b8f945f5fd&tid=381a4267ae613312e09aacb69326a680",
 
 
   ]
